4e6504723ff1ab3b8ff643dced5180c91cddd638
Katrina R <katrina.reinsdorff@gmail.com>
2019-07-23 19:47:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// add solution here
+function theBeatlesPlay(musicians, instruments){
+  var test = [];
+
+  for(var i = 0; i < musicians.length; i++) {
+    test.push (`${musicians[i]} plays ${ instruments[i]}`);
+  }
+  return test;
+}
+
+function johnLennonFacts (facts){
+  var x = 0;
+  var str = [];
+
+  while (facts[x]) {
+    str.push (facts[x] + "!!!");
+    x++;
+  }
+  return(str);
+}
+
+function iLoveTheBeatles(number) {
+  var z = [];
+  
+  
+}
